5e7e8ea440688e7797252942b1ad816397b1972c
Naveen <noobimplementsjs@gmail.com>
2022-06-12 12:51:47 +0530

    Hover highlight feature

--- src/Summary.js
+++ src/Summary.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {Pie} from '@visx/shape';
 import {Group} from '@visx/group';
 import {Text} from '@visx/text';
+import {Connector} from '@visx/annotation';
 
 function Summary() {
 
@@ -20,15 +21,28 @@ function Summary() {
   const data = [
     { asset : 'RE', amount : 3600000},
     {asset : 'BI', amount : 1500000},
-    {asset :'Bul', amount: 500000, },
-    {asset:'CR', amount : 500000, },
-    {asset:'CRY', amount : 900000, },
-    {asset:'SE', amount : 300000, }
+    {asset :'Bul', amount: 500000},
+    {asset:'CR', amount : 500000},
+    {asset:'CRY', amount : 900000},
+    {asset:'SE', amount : 300000}
   ];
+
+  function addOutline(data) {
+    if (data.asset === active.asset){
+      return 'black'
+    }
+    else {
+      return 'none'
+    }
+  }
+
+  function pathCenter(pathElem) {
+    return {x:10, y : 50};
+  }
 
   const width = 400;
   return (
-    <svg width ={width} height = {width} >
+    <svg width ={width} height = {width}>
       <Group top = {width/2} left = {width/2}>
         <Pie data = {data} pieValue = {(data)=> data.amount} 
         outerRadius = {width/2  -3} innerRadius = {width/2 - 30}
@@ -36,15 +50,17 @@ function Summary() {
         padAngle = {0.005}>
           {(pie)=> {
             return pie.arcs.map((arc) =>{
-              // console.log(arc);
               return(
               <g key= {arc.data.asset} 
               onMouseEnter = {() => setActive(arc.data)}
               onMouseLeave = {() => setActive(null)}
               >
                 <path d= {pie.path(arc)} fill = {asset_color[arc.data.asset]} 
+                stroke = {active ? (addOutline(arc.data)): 'none'}
+                strokeWidth = {2}
                 >  
                 </path>
+                <Connector x = {-190} y = {0} dx = {-50} dy = {-10} stroke = 'black'/>
               </g>
               )
             })
