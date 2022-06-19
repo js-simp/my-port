a02983b771d7f6413a1937dd2c336ad6ed73de1e
Tharindu Jayakody <61074238+tharindudj@users.noreply.github.com>
2022-06-19 06:15:35 +0000

    Added carousel to Slides.js

--- src/Securities.js
+++ src/Securities.js
@@ -1,25 +1,17 @@
 //Securities investment Analytics
 import React from 'react';
-import Slider from "react-slick";
 import Slides from './Slides'
 
 function Securities() {
   const my_securities = [
-    {asset : 'Fixed Depost', initiated : '10/11/2021', period_in_years : 2, growth : 17.5, amount : '10,000'},
-    {asset : 'Money Market', initiated : '06/01/2020', period_in_years : 0.5, growth : 12, amount : '10,000'},
-    {asset : 'Treasury Bonds', initiated : '10/06/2020', period_in_years : 0.25, growth : 21, amount : '10,000'},
-    {asset : 'Fied Depost', initiated : '10/10/2021', period_in_years : 1, growth : 17, amount : '10,000'},
-    {asset : 'Insurance', initiated : '31/12/2021', period_in_years : 5, growth : 30, amount : '10,000'},
-    {asset : 'Index Fund', initiated : '10/11/2021', period_in_years : 10, growth : 18.9, amount : '10,000'}
+    {asset : 'Fixed Depost', id: 1, initiated : '10/11/2021', period_in_years : 2, growth : 17.5, amount : '10,000'},
+    {asset : 'Money Market', id: 2, initiated : '06/01/2020', period_in_years : 0.5, growth : 12, amount : '10,000'},
+    {asset : 'Treasury Bonds', id: 3, initiated : '10/06/2020', period_in_years : 0.25, growth : 21, amount : '10,000'},
+    {asset : 'Fied Depost', id: 4, initiated : '10/10/2021', period_in_years : 1, growth : 17, amount : '10,000'},
+    {asset : 'Insurance', id: 5, initiated : '31/12/2021', period_in_years : 5, growth : 30, amount : '10,000'},
+    {asset : 'Index Fund', id: 6, initiated : '10/11/2021', period_in_years : 10, growth : 18.9, amount : '10,000'}
 ]
 
-  const slider_settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-  }
 
   return (
     
